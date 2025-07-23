cb18b8cd40f24c54233a6318c671a8663d5294e9
AsaruAsharudheen <asaru313313@gmail.com>
2025-07-23 11:50:23 +0530

    Your update message

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,28 +1,33 @@
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import './Home.css'; // Import the CSS file
 
 const Home = () => {
   const [iframes, setIframes] = useState([]);
 
   useEffect(() => {
-    axios.get('https://iframe-backend.onrender.com/api/iframes')
+    axios
+      .get('https://iframe-backend.onrender.com/api/iframes')
       .then(res => setIframes(res.data))
       .catch(err => console.error(err));
   }, []);
 
   return (
-    <div style={{ padding: '20px' }}>
+    <div className="home-container">
       <h1>All 360° Links</h1>
-      <Link to="/add">
-        <button>Add New 360° URL</button>
-      </Link>
 
-      <div style={{ marginTop: '20px' }}>
+      <div className="add-button-container">
+        <Link to="/add">
+          <button className="add-button">+ Add New 360° URL</button>
+        </Link>
+      </div>
+
+      <div className="iframe-list">
         {iframes.map(iframe => (
-          <div key={iframe._id} style={{ marginBottom: '20px' }}>
+          <div key={iframe._id} className="iframe-card">
             <p>
-              Share link:{' '}
+              <strong>Share link:</strong>{' '}
               <a
                 href={`/view/${iframe.uniqueId}`}
                 target="_blank"
@@ -31,7 +36,6 @@ const Home = () => {
                 {window.location.origin}/view/{iframe.uniqueId}
               </a>
             </p>
-            <p style={{ fontSize: '12px' }}>URL: {iframe.url}</p>
           </div>
         ))}
       </div>
